682e1b5e67ee47f6f7e001c24df4a9f0acac5891
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-08-11 18:01:18 +0300

    Add Custom Templator

---
+++ utils/common.js
@@ -0,0 +1,22 @@
+const testTempl = `
+<div>
+        {{ field1 }}
+        <span>{{field2}}</span>
+        <span>{{ field3.info.name }}</span>
+</div>
+`;
+const tmpl = new Templator(testTempl);
+
+const context = {
+    field1: 'Text 1',
+    field2: 42,
+    field3: {
+        info: {
+            name: 'Simon',
+        },
+    },
+};
+
+const renderedTemplate = tmpl.compile(context); // Строка с html-вёрсткой
+
+document.body.innerHTML = renderedTemplate; // Показался нужный результат
