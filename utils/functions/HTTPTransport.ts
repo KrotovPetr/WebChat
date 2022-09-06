83feee187154f72e5e7464807429a3e0849739bd
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-09-06 09:24:08 +0300

    Fix some bugs

--- utils/functions/HTTPTransport.ts
+++ utils/functions/HTTPTransport.ts
@@ -1,73 +1,73 @@
-// import { queryStringify } from './queryStringify';
-//
-// type TMethods = {
-//     GET: string,
-//     PUT: string,
-//     DELETE: string,
-//     POST: string,
-// }
-//
-// type TOptions = {
-//     headers?: any,
-//     method?: string,
-//     data?: any,
-//     timeout: number
-// }
-//
-// const METHODS:TMethods = {
-//     GET: 'GET',
-//     PUT: 'PUT',
-//     DELETE: 'DELETE',
-//     POST: 'POST',
-// };
-//
-//
-//
-// class HTTPTransport {
-//     get = (url: string, options: {} | TOptions = {}) => {
-//         let dataOp = queryStringify(options.data);
-//         return this.request(url+dataOp, {...options,  method: METHODS.GET}, options.timeout);
-//     };
-//
-//     put=(url: string, options={})=>{
-//         let dataOp = queryStringify(options.data);
-//         return this.request(url, {...options ,  data: dataOp, method: METHODS.GET}, options.timeout);
-//     }
-//     post=(url: string,options={})=>{
-//         let dataOp = queryStringify(options.data);
-//         return this.request(url, {...options,  data: dataOp, method: METHODS.GET}, options.timeout);
-//
-//     }
-//     delete=(url: string,options={})=>{
-//         let dataOp = queryStringify(options.data);
-//         return this.request(url, {...options,  data: dataOp, method: METHODS.GET}, options.timeout);
-//     }
-//
-//     request = (url: string, options, timeout = 5000) => {
-//         return new Promise((resolve, reject) => {
-//             const xhr = new XMLHttpRequest();
-//             xhr.open(options.method, url);
-//             if(options.headers){
-//                 for(let key in options.headers){
-//                     xhr.setRequestHeader(key, options.headers[key]);
-//                 }
-//             }
-//
-//             xhr.onload = function() {
-//                 resolve(xhr);
-//             };
-//
-//             xhr.onabort = reject;
-//             xhr.onerror = reject;
-//             xhr.ontimeout = reject;
-//
-//             if (options.method === METHODS.GET || !options.data) {
-//                 xhr.send();
-//             } else {
-//                 xhr.send(options.data);
-//             }
-//
-//         });
-//
-//     };
-// }
+import { queryStringify } from './queryStringify';
+
+type TMethods = {
+    GET: string,
+    PUT: string,
+    DELETE: string,
+    POST: string,
+}
+
+type TOptions = {
+    headers?: any,
+    data?: any,
+    timeout?: number | string,
+    method?: string;
+}
+
+const METHODS:TMethods = {
+    GET: 'GET',
+    PUT: 'PUT',
+    DELETE: 'DELETE',
+    POST: 'POST',
+};
+
+
+
+class HTTPTransport {
+    get = (url: string, options: {} | TOptions = {}) => {
+        let dataOp = queryStringify(options.data);
+        return this.request(url+dataOp, {...options,  method: METHODS.GET}, options.timeout);
+    };
+
+    put=(url: string, options={})=>{
+        let dataOp = queryStringify(options.data);
+        return this.request(url, {...options ,  data: dataOp, method: METHODS.GET}, options.timeout);
+    }
+    post=(url: string,options={})=>{
+        let dataOp = queryStringify(options.data);
+        return this.request(url, {...options,  data: dataOp, method: METHODS.GET}, options.timeout);
+
+    }
+    delete=(url: string,options={})=>{
+        let dataOp = queryStringify(options.data);
+        return this.request(url, {...options,  data: dataOp, method: METHODS.GET}, options.timeout);
+    }
+
+    request = (url: string, options: TOptions, timeout: number = 5000) => {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open(options.method, url);
+            if(options.headers){
+                for(let key in options.headers){
+                    xhr.setRequestHeader(key, options.headers[key]);
+                }
+            }
+
+            xhr.onload = function() {
+                resolve(xhr);
+            };
+
+            xhr.onabort = reject;
+            xhr.onerror = reject;
+            xhr.ontimeout = reject;
+
+            if (options.method === METHODS.GET || !options.data) {
+                xhr.send();
+            } else {
+                xhr.send(options.data);
+            }
+
+        });
+
+    };
+}
