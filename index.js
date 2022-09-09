37360ee2feb0efce1f6714e516790ec25a362c76
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-09-10 00:29:33 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
 
+const express = require('express');
+require('dotenv').config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
